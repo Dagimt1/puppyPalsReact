71a775f092beeb78292b33ec2601846f118d1021
Dagim Jalleta <dagimteriessa@gmail.com>
2024-04-29 20:40:29 -0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
-import { useState } from 'react'
-import './App.css'
-import {puppyList} from './data.js'
+import { useState } from 'react';
+import './App.css';
+import { puppyList } from './data.js';
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
+  
+  }
+
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+
+  console.log(featuredPup);
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
-export default App
+
+export default App;
